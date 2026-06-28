b36eff01839b7849c4025f709cad600615ffb300
1Q08 <19872205914@163.com>
2026-06-29 03:25:48 +0800

    增加 ./tools/system_update.py 脚本文件，Ubuntu 系统软件维护

--- ros2_python_node.py
+++ ros2_python_node.py
@@ -3,11 +3,11 @@ from rclpy.node import Node
 
 def main():
     rclpy.init()  # 初始化工作，分配资源
-    node = Node("python_node")
-    node.get_logger().info("Hello, ROS2 from Python!")
-    node.get_logger().warn("Hello, ROS2 from Python!")
-    node.get_logger().error("Hello, ROS2 from Python!")
-    rclpy.spin(node)
+    node = Node("python_node")  # 创建一个名为 "python_node" 的节点
+    node.get_logger().info("Hello, ROS2 from Python!")  # 打印日志信息
+    node.get_logger().warn("Hello, ROS2 from Python!")  # 打印警告信息
+    node.get_logger().error("Hello, ROS2 from Python!")  # 打印错误信息
+    rclpy.spin(node)  # 进入循环，等待回调函数的触发
     rclpy.shutdown()  # 清理工作，释放资源
 
 if __name__ == "__main__":
--- tools/README.md
+++ tools/README.md
@@ -5,9 +5,11 @@
 **ROS1/ROS2：**
 
 > - [PlotJuggler](https://github.com/PlotJuggler/PlotJuggler)：可视化时间序列的工具。
+> - RQt：基于 Qt 图形框架为 ROS 开发的一套图形用户界面工具集和插件开发框架。
 
 ------
 
 ## 自己写的脚本
 
-> - sysinfo.sh：显示 ROS1/2 版本、架构、Ubuntu 版本、磁盘使用
+> - sysinfo.sh：显示 ROS1/2 版本、架构、Ubuntu 版本、磁盘使用。
+> - system_update.py：Ubuntu 系统维护脚本。
